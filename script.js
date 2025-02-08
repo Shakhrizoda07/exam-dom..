83391c0cfc95afbb1d3d264db360e2702cdef196
Shakhrizoda07 <shakhrizoda752@gmail.com>
2025-02-08 03:47:04 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const name = prompt('Ismingiz nima?');
+const age = prompt('Yoshingiz nechchida?');
+
+
+
+document.querySelector('#name').textContent = name;
+document.querySelector('#age').textContent = age;
+
+
+const resultElement = document.querySelector('#result');
